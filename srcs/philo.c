b1ec5ddad08d73d979f37806ae72754c0b4023a5
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-04-24 17:23:02 +0200

    end day, i was doing exercises (i'm on exo 4) from gpt to understand how to use threads and mutex

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,18 +6,27 @@
 /*   By: lefoffan <lefoffan@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 17:24:57 by lefoffan          #+#    #+#             */
-/*   Updated: 2025/04/24 12:09:02 by lefoffan         ###   ########.fr       */
+/*   Updated: 2025/04/24 13:45:14 by lefoffan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
+
+int counter = 0;
+pthread_mutex_t	mutex;
 
 void	*routine(void *args)
 {
 	int	tid;
 
 	tid = *(int *) args;
-	printf("Hello from philo %d\n", tid);
+	if (pthread_mutex_lock(&mutex) != 0)
+		return (NULL);
+	for (int i = 0; i < 1000000; i++)
+		++counter;
+	printf("counter = %d\n", counter);
+	if (pthread_mutex_unlock(&mutex) != 0)
+		return (NULL);
 	return (NULL);
 }
 
@@ -30,12 +39,15 @@ int	main(int ac, char **av)
 
 	if (args_errors_handler(ac, av) != 0)
 		return (1);
+	
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+		return (print_error("failed init mutex"), 1);
 
 	nt = ft_atoi(av[1]);
-	threads = ft_calloc(sizeof(pthread_t), (nt + 1));
+	threads = ft_calloc(sizeof(pthread_t), nt);
 	if (!threads)
 		return (print_error("ft_calloc failed"), 1);
-	tids = ft_calloc(sizeof(int), (nt + 1));
+	tids = ft_calloc(sizeof(int), nt);
 	if (!tids)
 		return (print_error("ft_calloc failed"), 1);
 
@@ -43,15 +55,20 @@ int	main(int ac, char **av)
 	{
 		tids[i] = i + 1;
 		if (pthread_create(&threads[i], NULL, routine, &tids[i]) != 0)
-			return (print_error("Thread creation failed"), free(threads), 1);
+			return (print_error("Thread creation failed"), free(threads), free(tids), 1);
 	}
 
 	for (int i = 0; i < nt; i++)
 	{
 		if (pthread_join(threads[i], NULL) != 0)
-			return (print_error("Thread join failed"), free(threads), 1);
+			return (print_error("Thread join failed"), free(threads), free(tids), 1);
 	}
 
+	free(threads);
+	free(tids);
+
+	if (pthread_mutex_destroy(&mutex) != 0)
+		return (print_error("destroying mutex failed"), 1);
 	// init_philos(philos, ft_atoi(av[1]), routine, philos);
 	// if (!philos)
 	// 	return (1);
---
+++ test.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+pthread_mutex_t	atm;
+
+int	main(void)
+{
+	pthread_t	clients[3];
+
+	for (int i = 0; i < 3; i++)
+	
+}
